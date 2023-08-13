b5f5acb0a0ed3ea22c4b11cb2aeafd7859933f2c
Ido Flax <idoflax@gmail.com>
2023-08-13 22:31:36 +0200

    using dedicated actions written in TS

---
+++ .github/actions/detect-modified-projects/dist/index.js
@@ -0,0 +1,64 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = require("child_process");
+const core = __importStar(require("@actions/core"));
+function run() {
+    var _a, _b;
+    try {
+        const subprojectPrefixes = (_b = (_a = core.getInput('project_prefixes')) === null || _a === void 0 ? void 0 : _a.split(",")) !== null && _b !== void 0 ? _b : [];
+        core.debug("executing git fetch");
+        (0, child_process_1.execSync)('git fetch');
+        const githubSha = process.env.GITHUB_SHA;
+        if (!githubSha) {
+            core.setFailed('GITHUB_SHA not set');
+        }
+        let modifiedProjects = (0, child_process_1.execSync)(`git diff --name-only origin/main..${githubSha}`, { encoding: 'utf8' });
+        if (modifiedProjects.includes('buildSrc/') && !modifiedProjects.includes('ktor-')) {
+            core.debug("only buildSrc has modified");
+            modifiedProjects = "buildSrc";
+        }
+        else {
+            const subprojectPrefixesPattern = subprojectPrefixes.join("|");
+            const regex = subprojectPrefixes.length > 0
+                ? new RegExp(`^(${subprojectPrefixesPattern})`)
+                : null;
+            modifiedProjects = modifiedProjects.split('\n')
+                .filter(line => {
+                return regex ? regex.test(line) : true;
+            })
+                .map(line => line.split('/', 1)[0])
+                .sort()
+                .filter((value, index, self) => self.indexOf(value) === index)
+                .join(',');
+        }
+        core.debug(`Modified subprojects: ${modifiedProjects}`);
+        core.setOutput('modified_projects', modifiedProjects);
+    }
+    catch (error) {
+        core.setFailed(`Action failed with error: ${error}`);
+    }
+}
+run();
---
+++ .github/actions/execute-gradle-tasks-on-projects/dist/index.js
@@ -0,0 +1,72 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const child_process_1 = require("child_process");
+function run() {
+    try {
+        const projects = core.getInput('projects');
+        const tasks = core.getInput('tasks');
+        const parentProjectTask = core.getInput('parent_project_task');
+        core.debug(`Projects: ${projects}`);
+        core.debug(`Tasks: ${tasks}`);
+        core.debug(`Parent Project Task: ${parentProjectTask}`);
+        let gradleProjectsTasks;
+        if (projects === 'buildSrc') {
+            core.debug(`only buildSrc has changed, setting gradleProjectsTasks to ${tasks}`);
+            gradleProjectsTasks = `${tasks} `;
+        }
+        else {
+            core.debug(`building gradleProjectsTasks`);
+            const projArr = projects.split(',');
+            const taskArr = tasks.split(',');
+            gradleProjectsTasks = projArr.reduce((acc1, proj) => {
+                return acc1 + taskArr.reduce((acc2, task) => {
+                    return acc2 + `:${proj}:${task} `;
+                }, '');
+            }, '');
+        }
+        gradleProjectsTasks += `${parentProjectTask} `;
+        const gradleCommand = `./gradlew --stacktrace --console=plain ${gradleProjectsTasks.trim()}`;
+        core.debug(`Executing: ${gradleCommand}`);
+        const gradleArgs = gradleCommand.split(' ');
+        const gradleChild = (0, child_process_1.spawn)(gradleArgs[0], gradleArgs.slice(1));
+        gradleChild.on('message', (message) => {
+            core.debug(message.toString());
+        });
+        gradleChild.on('exit', (code, signal) => {
+            if (code !== 0) {
+                core.setFailed(`Gradle exited with code ${code} due to signal ${signal}`);
+            }
+            else {
+                core.setOutput('gradle_output', gradleChild.stdout);
+            }
+        });
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+run();

37f094d2a219c7f6e3121258c80356efe5dd15a7
Ido Flax <idoflax@gmail.com>
2023-09-14 15:47:16 +0200

    remove examples

--- examples/src/main/kotlin/flaxoos/github/io/domain/User.kt
+++
@@ -1,9 +0,0 @@
-package flaxoos.github.io.domain
-
-import com.sksamuel.avro4k.AvroNamespace
-import kotlinx.serialization.Serializable
-
-@Suppress("PROVIDED_RUNTIME_TOO_LOW")
-@Serializable
-@AvroNamespace("flaxoos.github.io.domain")
-data class User(val id: String, val username: String)
--- examples/src/test/kotlin/flaxoos/github/io/ApplicationTest.kt
+++
@@ -1,4 +0,0 @@
-package flaxoos.github.io
-
-class ApplicationTest {
-}

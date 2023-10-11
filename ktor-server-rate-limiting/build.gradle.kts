12cccb305b6fe2c6519176bdb96ae774a2671b09
Ido Flax <idoflax@gmail.com>
2023-10-11 18:25:27 +0200

    feat(ios-client-plugins): recreate release

--- ktor-server-rate-limiting/build.gradle.kts
+++ ktor-server-rate-limiting/build.gradle.kts
@@ -42,4 +42,3 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
         freeCompilerArgs += "-Xcontext-receivers"
         freeCompilerArgs += "-Xuse-ir"
     }
-}

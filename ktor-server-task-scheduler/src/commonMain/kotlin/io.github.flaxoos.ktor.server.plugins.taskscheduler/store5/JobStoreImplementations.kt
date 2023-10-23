c9f096cd8dddd2bd3f4275f9601bb2a1d679f470
Ido Flax <idoflax@gmail.com>
2023-10-23 23:38:05 +0200

    feat(job-store): public api

--- ktor-server-task-scheduler/src/commonMain/kotlin/io.github.flaxoos.ktor.server.plugins.taskscheduler/store5/JobStoreImplementations.kt
+++ ktor-server-task-scheduler/src/commonMain/kotlin/io.github.flaxoos.ktor.server.plugins.taskscheduler/store5/JobStoreImplementations.kt
@@ -3,6 +3,7 @@ package io.github.flaxoos.ktor.server.plugins.taskscheduler.store5
 import com.benasher44.uuid.Uuid
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.flow
+import org.mobilenativefoundation.store.store5.ExperimentalStoreApi
 import org.mobilenativefoundation.store.store5.Fetcher
 import org.mobilenativefoundation.store.store5.MutableStore
 import org.mobilenativefoundation.store.store5.OnUpdaterCompletion
@@ -10,77 +11,82 @@ import org.mobilenativefoundation.store.store5.SourceOfTruth
 import org.mobilenativefoundation.store.store5.StoreBuilder
 import org.mobilenativefoundation.store.store5.Updater
 import org.mobilenativefoundation.store.store5.UpdaterResult
-
-fun provide(
-    api: JobApi
-): Fetcher<JobKey, JobNetwork> = Fetcher.of { key ->
-    require(key is JobKey.Read)
-    when (key) {
-        is JobKey.Read.ById -> api.getJobById(key.jobId)
-    }
-}
-
-fun provide(
-    api: JobApi
-): Updater<JobKey, JobCommon, JobUpdaterResult> =
-    Updater.by(
-        post = { key, input ->
-            require(key is JobKey.Write)
-            when (key) {
-                is JobKey.Write.Create -> api.create(input)
-                is JobKey.Write.ById -> api.update(key.jobId, input)
-            }
-        },
-        onCompletion = OnUpdaterCompletion(
-            onSuccess = { success: UpdaterResult.Success ->
-                UserLogger.post(StoreEvents.Update(success.status))
-            },
-            onFailure = { failure: UpdaterResult.Error ->
-                UserLogger.post(StoreEvents.Update(failure.status))
-            }
-        )
-    )
-
-fun provide(
-    db: JobDb
-): SourceOfTruth<JobKey, Job, Job> = SourceOfTruth.of(
-    reader = { key: JobKey ->
-        require(key is JobKey.Read)
-        flow {
-            when (key) {
-                is JobKey.Read.ById -> emit(db.getJobById(key.JobId))
-            }
-        }
-    },
-    writer = { key, input ->
-        require(key is JobKey.Write)
-        when (key) {
-            is JobKey.Write.Create -> db.create(input)
-            is JobKey.Write.ById -> db.update(key.JobId, input)
-        }
-    },
-    delete = { key: JobKey ->
-        require(key is JobKey.Clear.ById)
-        db.deleteById(key.JobId)
-    },
-    deleteAll = db.delete()
-)
-
-fun provide(
-    fetcher: Fetcher<JobKey, JobNetwork>,
-    sourceOfTruth: SourceOfTruth<JobKey, Job, Job>,
-    updater: Updater<JobKey, JobCommon, JobUpdaterResult>,
-): MutableStore<JobKey, JobCommon> = StoreBuilder.from(
-    fetcher = fetcher,
-    sourceOfTruth = sourceOfTruth,
-).build(updater)
-
-interface JobApi {
-    fun getJobById(jobId: Uuid): JobNetwork
-    fun create(input: JobCommon): UpdaterResult
-    fun update(jobId: Uuid, input: JobCommon): UpdaterResult
-}
-
-interface JobDb {
-    fun getJobById(jobId: Uuid): Job
-}
+//
+//public fun fetcher(
+//    api: JobApi
+//): Fetcher<JobKey, JobNetwork> = Fetcher.of { key ->
+//    require(key is JobKey.Read)
+//    when (key) {
+//        is JobKey.Read.ById -> api.getJobById(key.jobId)
+//    }
+//}
+//
+//public fun updater(
+//    api: JobApi
+//): Updater<JobKey, JobCommon, JobUpdaterResult> =
+//    Updater.by(
+//        post = { key, input ->
+//            require(key is JobKey.Write)
+//            when (key) {
+//                is JobKey.Write.Create -> api.create(input)
+//                is JobKey.Write.ById -> api.update(key.jobId, input)
+//            }
+//        },
+//        onCompletion = OnUpdaterCompletion(
+//            onSuccess = { success: UpdaterResult.Success ->
+//                UserLogger.post(StoreEvents.Update(success.status))
+//            },
+//            onFailure = { failure: UpdaterResult.Error ->
+//                UserLogger.post(StoreEvents.Update(failure.status))
+//            }
+//        )
+//    )
+//
+//public fun sourceOfTruth(
+//    db: JobDb
+//): SourceOfTruth<JobKey, Job, Job> = SourceOfTruth.of(
+//    reader = { key: JobKey ->
+//        require(key is JobKey.Read)
+//        flow {
+//            when (key) {
+//                is JobKey.Read.ById -> emit(db.getJobById(key.jobId))
+//            }
+//        }
+//    },
+//    writer = { key, input ->
+//        require(key is JobKey.Write)
+//        when (key) {
+//            is JobKey.Write.Create -> db.create(input)
+//            is JobKey.Write.ById -> db.update(key.jobId, input)
+//        }
+//    },
+//    delete = { key: JobKey ->
+//        require(key is JobKey.Clear.ById)
+//        db.deleteById(key.jobId)
+//    },
+//    deleteAll = db.delete()
+//)
+//
+//@OptIn(ExperimentalStoreApi::class)
+//public fun mutableStore(
+//    fetcher: Fetcher<JobKey, JobNetwork>,
+//    sourceOfTruth: SourceOfTruth<JobKey, Job, Job>,
+//    updater: Updater<JobKey, JobCommon, JobUpdaterResult>,
+//): MutableStore<JobKey, JobCommon> = StoreBuilder.from(
+//    fetcher = fetcher,
+//    sourceOfTruth = sourceOfTruth,
+//).build(updater)
+//
+//public interface JobApi {
+//    public fun getJobById(jobId: Uuid): JobNetwork
+//    public fun create(input: JobCommon): UpdaterResult
+//    public fun update(jobId: Uuid, input: JobCommon): UpdaterResult
+//}
+//
+//public interface JobDb {
+//    public fun getJobById(jobId: Uuid): Job
+//    public fun create(input: JobCommon): Job
+//    public fun update(jobId: Uuid, input: JobCommon): Job
+//    public fun delete()
+//    public fun deleteById(jobId: Uuid)
+//}
--- ktor-server-task-scheduler/src/commonMain/kotlin/io.github.flaxoos.ktor.server.plugins.taskscheduler/store5/JobStoreTypes.kt
+++ ktor-server-task-scheduler/src/commonMain/kotlin/io.github.flaxoos.ktor.server.plugins.taskscheduler/store5/JobStoreTypes.kt
@@ -2,40 +2,40 @@ package io.github.flaxoos.ktor.server.plugins.taskscheduler.store5
 
 import com.benasher44.uuid.Uuid
 
-sealed class JobKey {
-    sealed class Read : JobKey() {
-        data class ById(val jobId: Uuid) : Read()
+public sealed class JobKey {
+    public sealed class Read : JobKey() {
+        public data class ById(val jobId: Uuid) : Read()
     }
 
-    sealed class Write : JobKey() {
-        data object Create : Write()
-        data class ById(val jobId: Uuid) : Write()
+    public sealed class Write : JobKey() {
+        public data object Create : Write()
+        public data class ById(val jobId: Uuid) : Write()
     }
 
-    sealed class Clear : JobKey() {
-        data object All : Clear()
-        data class ById(val jobId: Uuid) : Clear()
+    public sealed class Clear : JobKey() {
+        public data object All : Clear()
+        public data class ById(val jobId: Uuid) : Clear()
     }
 }
 
-data class JobNetwork(
+public data class JobNetwork(
     val id: Uuid
 )
 
-data class JobCommon(
+public data class JobCommon(
     val id: Uuid
 )
 
-sealed class JobUpdaterResult {
-    abstract val status: Int
+public sealed class JobUpdaterResult {
+    public abstract val status: Int
 
-    sealed class Success : JobUpdaterResult() {
-        data class Ok(override val status: Int) : Success()
-        data class Created(override val status: Int) : Success()
+    public sealed class Success : JobUpdaterResult() {
+        public data class Ok(override val status: Int) : Success()
+        public data class Created(override val status: Int) : Success()
     }
 
-    sealed class Failure : JobUpdaterResult() {
-        data class ClientError(override val status: Int) : Failure()
-        data class ServerError(override val status: Int) : Failure()
+    public sealed class Failure : JobUpdaterResult() {
+        public data class ClientError(override val status: Int) : Failure()
+        public data class ServerError(override val status: Int) : Failure()
     }
 }

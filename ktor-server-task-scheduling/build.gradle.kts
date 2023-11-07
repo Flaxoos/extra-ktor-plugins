c625042d1c491fb2a7797ca84212c1ffe534c9ee
Ido Flax <idoflax@gmail.com>
2023-11-08 00:55:39 +0100

    feat(task-scheduler): added base project

--- ktor-server-task-scheduling/build.gradle.kts
+++ ktor-server-task-scheduling/build.gradle.kts
@@ -1,1 +1,16 @@
-plugins { idea }
+import io.github.flaxoos.ktor.commonMainDependencies
+
+plugins {
+    id("ktor-server-plugin-conventions")
+}
+
+kotlin {
+    sourceSets {
+        commonMainDependencies {
+            api(projects.ktorServerTaskScheduling.ktorServerTaskSchedulingCore)
+            api(projects.ktorServerTaskScheduling.ktorServerTaskSchedulingRedis)
+            api(projects.ktorServerTaskScheduling.ktorServerTaskSchedulingJdbc)
+            api(projects.ktorServerTaskScheduling.ktorServerTaskSchedulingMongodb)
+        }
+    }
+}

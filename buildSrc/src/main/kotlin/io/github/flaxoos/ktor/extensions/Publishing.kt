e26a560dbbf8808da5beaa8c8d59b876173d1669
Ido Flax <idoflax@gmail.com>
2024-06-21 09:09:23 +0200

    feat(no-fat-jar): to fix #30 (dependency issues) - removed shadow usage

--- buildSrc/src/main/kotlin/io/github/flaxoos/ktor/extensions/Publishing.kt
+++ buildSrc/src/main/kotlin/io/github/flaxoos/ktor/extensions/Publishing.kt
@@ -1,21 +1,11 @@
 package io.github.flaxoos.ktor.extensions
 
-import com.github.jengelman.gradle.plugins.shadow.internal.DependencyFilter
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import groovy.util.NodeList
-import kotlinx.atomicfu.plugin.gradle.AtomicFUTransformTask
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.tasks.AbstractPublishToMaven
-import org.gradle.api.publish.maven.tasks.PublishToMavenLocal
-import org.gradle.api.tasks.TaskContainer
-import org.gradle.api.tasks.TaskProvider
 import org.gradle.configurationcache.extensions.capitalized
 import org.gradle.jvm.tasks.Jar
-import org.gradle.kotlin.dsl.assign
-import org.gradle.kotlin.dsl.create
-import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.the
@@ -23,13 +13,9 @@ import org.gradle.kotlin.dsl.withType
 import org.gradle.plugins.signing.Sign
 import org.gradle.plugins.signing.SigningExtension
 import org.jetbrains.dokka.gradle.AbstractDokkaTask
-import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.dokka.gradle.DokkaTaskPartial
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import java.util.Base64
 
 private const val JVM = "jvm"
-private fun shadowJvmJarTaskName() = "shadow${JVM.capitalized()}Jar"
 private const val DOKKA_JAR = "dokkaJar"
 private const val DOKKA_HTML = "dokkaHtml"
 
@@ -101,75 +87,3 @@ fun Project.configureMavenPublications() {
         }
     }
 }
-
-/**
- * Creates a shadow JAR task for the JVM target.
- */
-fun Project.createJvmShadowJar(dependencyFilter: DependencyFilter.() -> Unit = {}) {
-    val mainCompilation = with(the<KotlinMultiplatformExtension>()) {
-        targets.findByName(JVM)?.compilations?.getByName("main")
-    }?.let { provider { it } } ?: return
-    val sourceJar = tasks.register("sourceJar", org.gradle.api.tasks.bundling.Jar::class) {
-        from(mainCompilation.map { it.allKotlinSourceSets.map { kotlinSourceSet -> kotlinSourceSet.kotlin } })
-        archiveClassifier.set("sources")
-    }
-    val shadowJvmJar = tasks.register(shadowJvmJarTaskName(), ShadowJar::class) {
-        archiveBaseName = "${project.name}-$JVM"
-        from(mainCompilation.map { it.output })
-        archiveClassifier.set("")
-        configurations = listOf(
-            project.configurations["${JVM}CompileClasspath"],
-            project.configurations["${JVM}RuntimeClasspath"],
-        )
-        dependsOn(tasks.named("jvmJar"))
-        dependencies {
-            dependencyFilter()
-        }
-        mustRunAfter(tasks.named("generateMetadataFileForJvmPublication"))
-        mustRunAfter(tasks.named("signJvmPublication"))
-    }
-    tasks.withType<DokkaTaskPartial>().configureEach {
-        dependsOn(shadowJvmJar)
-    }
-    with(the<PublishingExtension>()) {
-        publications {
-            create("shadow${JVM.capitalized()}", MavenPublication::class) {
-                artifact(shadowJvmJar.map { it.archiveFile })
-                artifact(sourceJar)
-                artifact(tasks.named(DOKKA_JAR))
-                artifactId = "${project.name}-$JVM"
-                pom.withXml {
-                    asNode().appendNode("repositories").apply {
-                        appendNode("repository").apply {
-                            appendNode("maven") {
-                                appendNode("url", "https://packages.confluent.io/maven/")
-                                appendNode("url", "https://packages.confluent.io/maven/")
-                            }
-                        }
-                    }
-                    (asNode().get("dependencies") as NodeList).clear()
-                }
-            }
-        }
-    }
-    tasks.withType(AbstractPublishToMaven::class) {
-        if (publication?.name?.contains(JVM, ignoreCase = true) == true
-            && publication?.name?.contains("shadow", ignoreCase = true) != true
-        ) {
-            enabled = false
-        }
-    }
-    tasks.withType(DokkaTask::class).configureEach {
-        dependsOn(shadowJvmJar)
-    }
-    tasks.withType(AtomicFUTransformTask::class).configureEach {
-        dependsOn(shadowJvmJar)
-    }
-}
-
-val TaskContainer.shadowJvmJar: TaskProvider<Jar>
-    get() = named(shadowJvmJarTaskName(), Jar::class)
-
-val TaskContainer.publishShadowJvmPublicationToMavenLocal: TaskProvider<PublishToMavenLocal>
-    get() = named("publishShadowJvmPublicationToMavenLocal", PublishToMavenLocal::class)
-

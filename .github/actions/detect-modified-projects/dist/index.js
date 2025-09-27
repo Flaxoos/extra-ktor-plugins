a55eadd0e6bf641a9795cba3940867e6535f51df
Ido Flax <idoflax@gmail.com>
2025-09-27 23:03:30 +0200

    ops(ci): conventional-commits based release versions
    - added enforcing of conventional commits
    - project version is now set by axion-release gradle plugin, configured to extract the version based on the commit history
    
    see https://axion-release-plugin.readthedocs.io/en/latest/

---
+++ .github/actions/detect-modified-projects/dist/index.js
@@ -0,0 +1,77 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = require("child_process");
+const core = __importStar(require("@actions/core"));
+function run() {
+    var _a, _b, _c, _d;
+    try {
+        const subprojectPrefixes = (_b = (_a = core.getInput('project_prefixes')) === null || _a === void 0 ? void 0 : _a.split(",")) !== null && _b !== void 0 ? _b : [];
+        const requiredProjects = (_d = (_c = core.getInput('required_projects')) === null || _c === void 0 ? void 0 : _c.split(",")) !== null && _d !== void 0 ? _d : [];
+        core.debug("executing git fetch");
+        (0, child_process_1.execSync)('git fetch --unshallow', { encoding: 'utf-8' });
+        const githubSha = process.env.GITHUB_SHA;
+        if (!githubSha) {
+            core.setFailed('GITHUB_SHA not set');
+        }
+        const diffCmd = `git diff --name-only HEAD~1..${githubSha}`;
+        core.debug(`Executing: ${diffCmd}`);
+        core.debug(`Git Status: ${(0, child_process_1.execSync)(`git status`, { encoding: 'utf-8' }).trim()}`);
+        core.debug(`SHA Exists: ${(0, child_process_1.execSync)(`git cat-file -e ${githubSha}`, { encoding: 'utf-8' }).trim()}`);
+        let modifiedProjects = (0, child_process_1.execSync)(diffCmd, { encoding: 'utf8' });
+        core.debug("Modified Projects:" + modifiedProjects);
+        core.debug("Required Projects:" + requiredProjects);
+        if (modifiedProjects.includes('buildSrc/') && !modifiedProjects.includes('ktor-')) {
+            core.debug("only buildSrc has modified");
+            modifiedProjects = "buildSrc";
+        }
+        else {
+            const subprojectPrefixesPattern = subprojectPrefixes.join("|");
+            core.debug("subprojectPrefixesPattern: " + subprojectPrefixesPattern);
+            const regex = subprojectPrefixes.length > 0
+                ? new RegExp(`^(${subprojectPrefixesPattern})`)
+                : null;
+            let modifiedProjectsArray = modifiedProjects.split('\n')
+                .filter(line => {
+                return regex ? regex.test(line) : true;
+            })
+                .map(line => line.split('/', 1)[0])
+                .sort()
+                .filter((value, index, self) => self.indexOf(value) === index);
+            modifiedProjects = [...new Set(modifiedProjectsArray.concat(requiredProjects))].join(',');
+        }
+        if (modifiedProjects) {
+            core.info(`Modified subprojects including required projects: ${modifiedProjects}`);
+            core.setOutput('modified_projects', modifiedProjects);
+        }
+        else {
+            core.info("No modified subprojects");
+        }
+    }
+    catch (error) {
+        core.setFailed(`Action failed with error: ${error}`);
+    }
+}
+run();
